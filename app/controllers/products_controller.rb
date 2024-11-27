bdbea778ae6161409bf56eedbf1890c338018de4
manabiii775 <ke218.nnkdal@icloud.com>
2024-11-27 12:37:30 +0900

    ActiveHashの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,23 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
   private
 
   def product_params
-    params.require(:product).params(
-      :name, :description, :category_id, :status_id,
-      :day_until_shipping_id, :shipping_cost_id, :prefecture_id, :price, :image
-    )merge(user_id: current_user.id)
+    params.require(:product).params(:name, :description, :category_id, :status_id,
+    :day_until_shipping_id, :shipping_cost_id, :prefecture_id, :price, :image).merge(user_id: current_user.id
+  )
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :day_until_shipping
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
