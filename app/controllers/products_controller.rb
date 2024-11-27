bf7c8dfc467269a8ba52810aa8f00ddb40ee803d
manabiii775 <ke218.nnkdal@icloud.com>
2024-11-27 10:31:30 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
   def new
   end
+
+
+  private
+
+  def product_params
+    params.require(:product).params(
+      :name, :description, :category_id, :status_id,
+      :day_until_shipping_id, :shipping_cost_id, :prefecture_id, :price, :image
+    )merge(user_id: current_user.id)
+  end
 end
