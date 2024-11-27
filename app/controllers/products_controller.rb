07e1097dce25c83da5fde68c9b773d2815ad9c0f
manabiii775 <ke218.nnkdal@icloud.com>
2024-11-27 17:31:20 +0900

    JavaScriptの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @product = Product.new
   end
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to  root_path(@prototype)
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).params(:name, :description, :category_id, :status_id,
+    params.require(:product).permit(:name, :description, :category_id, :status_id,
     :day_until_shipping_id, :shipping_cost_id, :prefecture_id, :price, :image).merge(user_id: current_user.id
   )
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,9 +9,16 @@ class Product < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_until_shipping_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
+  }
 end
